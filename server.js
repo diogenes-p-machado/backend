297c5f56145e22534b0948cce79e04ae486807d3
diogenes <diogenes.p.machado@gmail.com>
2022-09-14 13:59:41 -0400

    14/09/2022

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const db = new sqlite3.Database(':memory:')
 
 db.serialize(() => {
   db.run('ATTACH DATABASE db AS rh')
-  db.run('CREATE TABLE IF NOT EXISTS rh.pessoa (id_pessoa INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)')
-  db.run('CREATE TABLE IF NOT EXISTS rh.pai_filho (pai INTEGER, filho INTEGER)')
 })
 
 app.route('/:schema/:table')
@@ -21,7 +19,7 @@ app.route('/:schema/:table')
   .get((req, res) => {
     db.all(`select * from ${req.params.schema}.${req.params.table}`, (err, rows) => {
       rows.map((l) => {
-        l.id = String(l.id)
+        l[`id_${req.params.table}`] = String(l[`id_${req.params.table}`])
       })
 
       res.json(rows)
@@ -42,14 +40,14 @@ app.route('/:schema/:table')
 app.route('/:schema/:table/:id')
 
   .get((req, res) => {
-    db.get(`select * from ${req.params.schema}.${req.params.table} where id = ${req.params.id}`, (err, row) => {
-      row.id = String(row.id)
+    db.get(`select * from ${req.params.schema}.${req.params.table} where id_${req.params.table} = ${req.params.id}`, (err, row) => {
+      row[`id_${req.params.table}`] = String(row[`id_${req.params.table}`])
       res.json(row)
     })
   })
 
   .put((req, res) => {
-    db.run(`UPDATE ${req.params.schema}.${req.params.table} SET ${keysUpdate.join(", ")} WHERE id = ${req.params.id}`,
+    db.run(`UPDATE ${req.params.schema}.${req.params.table} SET ${keysUpdate.join(", ")} WHERE id_${req.params.table} = ${req.params.id}`,
       values
       , function () {
         res.setHeader("changes", this.changes)
@@ -58,7 +56,7 @@ app.route('/:schema/:table/:id')
   })
 
   .delete((req, res) => {
-    db.run(`DELETE FROM ${req.params.schema}.${req.params.table} WHERE id = ${req.params.id}`,
+    db.run(`DELETE FROM ${req.params.schema}.${req.params.table} WHERE id_${req.params.table} = ${req.params.id}`,
       function () {
         res.setHeader("changes", this.changes)
         res.sendStatus(204)
@@ -68,12 +66,13 @@ app.route('/:schema/:table/:id')
 app.route('/:schema/:table/:id/:other_table')
 
   .get((req, res) => {
-    db.all(`select * from ${req.params.schema}.${req.params.other_table} where id = ${req.params.id}`, (err, rows) => {
+    db.all(`select * from ${req.params.schema}.${req.params.other_table} where id_${req.params.table} = ${req.params.id}`, (err, rows) => {
       res.json(rows)
     })
   })
 
   .post((req, res) => {
+    req.body[`id_${req.params.table}`] = req.params.id
     let keys = Object.keys(req.body)
     let values = Object.values(req.body)
     db.run(`insert into ${req.params.schema}.${req.params.other_table} (${keys.join(", ")}) values (${keys.map(() => "?").join(", ")})`,
@@ -81,6 +80,7 @@ app.route('/:schema/:table/:id/:other_table')
       , (err) => {
         res.sendStatus(201)
       })
+    res.json(req.body)
 
   })
 
@@ -92,14 +92,14 @@ app.route('/:schema/:table/:id/:other_table/:id_other')
       res.sendStatus(201)
     })
   })
- 
+
   .delete((req, res) => {
     db.run(`delete from ${req.params.schema}.${req.params.table}_${req.params.other_table} as tbl
     where tbl.${req.params.table} = ${req.params.id} and ${req.params.other_table} = ${req.params.id_other}`, (err) => {
       res.sendStatus(201)
     })
   })
-  
+
 
 app.listen(port, () => {
   console.log(`Example app listening on host ${host} port ${port}`)
